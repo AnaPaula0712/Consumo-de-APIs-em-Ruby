a6e4814fb06f4b917c842474757d79e4909bf95b
Ana Westphalen <campos1984@hotmail.com>
2022-02-04 16:01:28 -0300

    Consumo de API do Github

---
+++ github.rb
@@ -0,0 +1,13 @@
+require 'rest-client'
+require 'json'
+
+url = 'https://api.github.com/search/repositories?'
+q = 'spotcode'
+language = 'ruby'
+
+resp = RestClient.get "#{url}q=#{q}&language=#{language}"
+
+puts resp.code
+puts JSON.parse(resp.body)
+puts JSON.parse(resp.body)['total_count']
+puts JSON.parse(resp.body)['items'][2]['description']
